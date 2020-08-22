f2664213b88375d7db8de8e542fafc73c8994ddd
Le-Code <1054145364@qq.com>
2020-08-22 10:41:42 +0800

    add solution13.java add function f1

---
+++ src/com/yaojian/Solution13.java
@@ -0,0 +1,12 @@
+public class Solution13 implements Solution {
+	
+	@override
+	public void solution(){
+		f1();
+	}
+	
+	public void f1(){
+		Sysout.out.println("hello, this is function made by git master");
+	}
+	
+}
