86d900ba4ce3c27e73d59347fc6a2cb0bae0fc0d
Le-Code <yaojian@192.168.0.112>
2021-07-11 14:35:04 +0800

    mac first commit empty 80

--- src/com/yaojian/Main.java
+++ src/com/yaojian/Main.java
@@ -1,10 +1,11 @@
 package com.yaojian;
 
-import com.yaojian.page2.Solution79_2;
+
+import com.yaojian.page2.Solution80;
 
 public class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution79_2();
+        Solution solution = new Solution80();
         solution.solution();
     }
 }
---
+++ src/com/yaojian/page2/Solution80.java
@@ -0,0 +1,11 @@
+package com.yaojian.page2;
+
+import com.yaojian.Solution;
+
+public class Solution80 implements Solution {
+
+    @java.lang.Override
+    public void solution() {
+        System.out.println("hello world");
+    }
+}
